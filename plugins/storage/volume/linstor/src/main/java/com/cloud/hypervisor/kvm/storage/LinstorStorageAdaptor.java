f7612bf87c1ed7829d92e17836736dfb8839ecef
Rene Peinthor <rene.peinthor@linbit.com>
2024-05-02 09:23:42 +0200

    linstor: disconnect-disk also search for resource name in Linstor

--- plugins/storage/volume/linstor/src/main/java/com/cloud/hypervisor/kvm/storage/LinstorStorageAdaptor.java
+++ plugins/storage/volume/linstor/src/main/java/com/cloud/hypervisor/kvm/storage/LinstorStorageAdaptor.java
@@ -315,7 +315,7 @@ public class LinstorStorageAdaptor implements StorageAdaptor {
                     null,
                     null);
 
-            optRsc = getResourceByPath(resources, volumePath);
+            optRsc = getResourceByPathOrName(resources, volumePath);
         } catch (ApiException apiEx) {
             // couldn't query linstor controller
             s_logger.error(apiEx.getBestMessage());
@@ -375,9 +375,10 @@ public class LinstorStorageAdaptor implements StorageAdaptor {
         return false;
     }
 
-    private Optional<ResourceWithVolumes> getResourceByPath(final List<ResourceWithVolumes> resources, String path) {
+    private Optional<ResourceWithVolumes> getResourceByPathOrName(
+            final List<ResourceWithVolumes> resources, String path) {
         return resources.stream()
-            .filter(rsc -> rsc.getVolumes().stream()
+            .filter(rsc -> getLinstorRscName(path).equalsIgnoreCase(rsc.getName()) || rsc.getVolumes().stream()
                 .anyMatch(v -> path.equals(v.getDevicePath())))
             .findFirst();
     }
